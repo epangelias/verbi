495c15f5633facf261ca886c52db0d4d68c2a2fa
Epangelias <epangelias@gmail.com>
2024-11-30 14:26:41 -0800

    fix logic

--- routes/api/get-info.tsx
+++ routes/api/get-info.tsx
@@ -9,7 +9,7 @@ export const handler = define.handlers({
   async POST(ctx) {
     const user = ctx.state.user;
     if (!user) throw new HttpError(STATUS_CODE.Unauthorized);
-    if (!(user.tokens >= 0 || user.isSubscribed)) throw new HttpError(STATUS_CODE.PaymentRequired);
+    if (!(user.tokens > 0 || user.isSubscribed)) throw new HttpError(STATUS_CODE.PaymentRequired);
     const { prompt } = await ctx.req.json();
     const completion = await generateChatCompletion(undefined, [{ role: 'system', content: prompt }]);
     await setUserData({ ...user, tokens: user.tokens - 1 });
